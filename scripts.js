b5b3b530f04f688da5169032afdf64dbdffa0f5d
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-02-03 10:22:35 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -62,3 +62,16 @@ return new Set(arr1.concat(arr2))
 }
 // equals to
 const symDiff = (s1, s2) => new Set([...s1, ...s2].filter(x => !s1.has(x) || !s2.has(x)));
+
+// --------------------- //
+function translateDate(dateStr) {
+  let str = dateStr.split("-").map(e => e.length > 2 ? (e.slice(0, 2) + " " + e.slice(2)).split(" ") : e)
+    .flat().map(n => +n + 50)
+    .map(s => String.fromCharCode(s))
+
+  return `${str[0]+str[1]}-${str[2]}-${str[3]}`
+}
+//equals to
+function translateDate(s) {
+  return s.replace(/\d\d/g, x => String.fromCharCode(+x + 50))
+}
