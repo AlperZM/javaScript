1dcca84bfb50bd65f3c0396a912c46336184994e
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-02-05 11:14:31 +0300

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -6,3 +6,7 @@ function isMagicThree(array) {
 }
 //equals to
 const isMagicThree = (a) => a.some((b) => a.some((c) => a.some((d) => b + c + d === 0)));
+
+// calculate GCD 
+//OBEB
+  let n = (a, b) => (b ? n(b, a % b) : a);
