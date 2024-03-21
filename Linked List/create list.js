74cdd3a1908f3724dc00667d7e0c670eca78b3e8
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-03-21 10:51:13 +0300

    Update create list.js

--- Linked List/create list.js
+++ Linked List/create list.js
@@ -10,6 +10,38 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  // for get the size of node
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next
+    }
+    return count
+  }
+  //This method empties out the list.
+  clear() {
+    this.head = null;
+  }
+
+  //This method returns the last node of the linked list.
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next
+      }
+    }
+    return lastNode
+  }
+
+  //This method returns the first node of the linked list.
+  getFirst() {
+    return this.head;
+  }
+
 }
 
 let node1 = new ListNode(2);
@@ -17,6 +49,14 @@ let node2 = new ListNode(3);
 let node3 = new ListNode(4);
 node1.next = node2; //this.next = null becomes next = node2 and so on....
 node2.next = node3
+
+let list = new LinkedList(node1) // create linked list
+
+console.log(
+  list.head.next.data //3 if use to list.head.next.next.data , output will be 4
+)
+
+console.log(list.size()) //3, size of the list
 
 console.log(
 
